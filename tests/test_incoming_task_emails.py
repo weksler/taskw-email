8476ef8691c2065cc89aab6aca7b560c9b4b0a6c
Michel Weksler <mweksler@gmail.com>
2022-07-06 22:48:33 -0700

    Basic layout of tests

---
+++ tests/test_incoming_task_emails.py
@@ -0,0 +1,25 @@
+import pytest
+import pytest_mock
+
+
+class TestIncomingTaskEmails:
+    def test_bad_creds(self):
+        pass
+
+    def test_no_inbox(self):
+        pass
+
+    def test_can_fetch_unread_messages(self):
+        pass
+
+    def test_no_unread_messages(self):
+        pass
+
+    def test_cant_fetch_message(self):
+        pass
+
+    def test_message_wrong_sender(self):
+        pass
+
+    def test_happy_path(self):
+        pass
